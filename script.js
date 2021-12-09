cc36e60807c480d079138b3a66b9efa14ce07fad
VeroniqueJakobs <93218554+VeroniqueJakobs@users.noreply.github.com>
2021-12-09 20:18:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// part 1
+const age = 14;
+
+if (age >= 18) {
+console.log("Welcome!");
+}  else{
+console.log("Sorry, no entrance. Please stay outside.") ;
+}
+
+// part 2
+const isFemale = true;
+
+if (isFemale) {
+console.log("You're welcome");
+} else {
+    console.log("Please stay outside");
+}
+
+// part 3
+const driverStatus = 'bob'
+
+if(driverStatus == 'bob'){
+console.log("You are allowed to drive");
+} else {
+console.log("You are not allowed to drive");
+}
